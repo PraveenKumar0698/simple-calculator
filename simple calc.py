ad06a748eff77c966fa761ecbb8d8c805453fd1c
Praveen Kumar <110919212+PraveenKumar0698@users.noreply.github.com>
2022-09-28 20:53:23 +0530

    Add files via upload

---
+++ simple calc.py
@@ -0,0 +1,21 @@
+a=int(input("Enter a number\n"))
+b=int(input("Enter a number\n"))
+print("::: What you wanna perform::::")
+c=input("enter the operation ")
+
+
+if c=='+':
+    d=a+b
+    print(d)
+elif c=="-":
+    d=a-b
+    print(d)
+elif c=="*":
+    d=a*b
+    print(d)
+elif c=='/':
+    d=a/b
+    print(d)
+elif c=='%':
+    d=a%b
+    print(d)
